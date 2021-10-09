7092c9785af175d0b3881a62afc13f50a1c60aa8
Paulus Esterhazy <pesterhazy@gmail.com>
2021-10-09 22:32:11 +0200

    WIP

--- src/test/todomvc/app_spec.cljs
+++ src/test/todomvc/app_spec.cljs
@@ -1,6 +1,5 @@
 (ns todomvc.app-spec
   (:require ["@testing-library/react" :as rtl]
-            ["@testing-library/jest-dom/extend-expect" :as for-side-effects]
             [uix.core.alpha :as uix]
             [todomvc.app :as x]))
 
