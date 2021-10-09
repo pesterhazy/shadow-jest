045bacce546768b551548cffc64d0d92409695f3
Paulus Esterhazy <pesterhazy@gmail.com>
2021-10-09 16:49:06 +0200

    WIP

--- src/test/todomvc/app_spec.cljs
+++ src/test/todomvc/app_spec.cljs
@@ -1,13 +1,12 @@
 (ns todomvc.app-spec)
 
 (js/console.log "XXX")
-(js/throw (js/Error. "boom"))
 
 ;; test('two plus two is four', () => {
 ;; expect(2 + 2).toBe(4);
 ;;});
 
-(js/test "arithmetic"
-         (fn []
-           (-> (js/expect (+ 2 2))
-               (.toBe 5))))
+#_(js/test "arithmetic"
+           (fn []
+             (-> (js/expect (+ 2 2))
+                 (.toBe 5))))
