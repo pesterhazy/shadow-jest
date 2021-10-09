ddab94b8302d5649c0e93290c69c3be1d1c455fd
Paulus Esterhazy <pesterhazy@gmail.com>
2021-10-09 22:52:08 +0200

    WIP

--- src/test/todomvc/app_spec.cljs
+++ src/test/todomvc/app_spec.cljs
@@ -3,8 +3,10 @@
             [uix.core.alpha :as uix]
             [todomvc.app :as x]))
 
-(js/test "shows string"
+(js/test "shows heading"
          (fn []
            (rtl/render (uix/as-element [x/app]))
            (-> (js/expect (rtl/screen.getByRole "heading"))
-               (.toHaveTextContent "todos"))))
+               (.toHaveTextContent "todos"))
+           (-> (js/expect (rtl/screen.getByPlaceholderText "What needs to be done?"))
+               (.toBeInTheDocument))))
