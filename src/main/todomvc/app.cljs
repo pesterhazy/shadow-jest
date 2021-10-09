7684d70e2adba200713134d35d05657bc3acc34f
Paulus Esterhazy <pesterhazy@gmail.com>
2021-10-09 21:28:14 +0200

    WIP

--- src/main/todomvc/app.cljs
+++ src/main/todomvc/app.cljs
@@ -1,3 +1,16 @@
-(ns todomvc.app)
+(ns todomvc.app
+  (:require [uix.core.alpha :as uix]
+            [uix.dom.alpha :as uix.dom]))
 
-(js/console.log "wip")
+(defn button [{:keys [on-click]} text]
+  [:button.btn {:on-click on-click}
+   text])
+
+(defn app []
+  (let [state* (uix/state 0)]
+    [:<>
+     [button {:on-click #(swap! state* dec)} "-"]
+     [:span @state*]
+     [button {:on-click #(swap! state* inc)} "+"]]))
+
+(uix.dom/render [app] js/root)
