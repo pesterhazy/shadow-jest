d096c3f63f0101b9b49cb7b0e5fa076b3d2456c8
Paulus Esterhazy <pesterhazy@gmail.com>
2022-03-16 17:57:29 +0100

    WIP

--- src/main/todomvc/main.cljs
+++ src/main/todomvc/main.cljs
@@ -1,6 +1,1 @@
-(ns todomvc.main
-  (:require [uix.dom.alpha :as uix.dom]
-            [todomvc.app :as app]))
-
-(uix.dom/render [app/app-ui {:initial-todos [{:label "Start REPL"}]}]
-                (js/document.querySelector "#app"))
+(ns todomvc.main)
