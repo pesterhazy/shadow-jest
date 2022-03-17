eaaace8d3dc232570f1a73d105ec815593b515e1
Paulus Esterhazy <pesterhazy@gmail.com>
2022-03-17 10:35:03 +0100

    WIP

--- src/main/tdd/infra/cmdline.cljs
+++ src/main/tdd/infra/cmdline.cljs
@@ -1,6 +1,15 @@
 (ns tdd.infra.cmdline)
 
-(defn create [])
+(defn create []
+  {:process js/process})
+
+(defn create-null []
+  (let [!last-output (atom nil)]
+    {:process #js{:stdout #js{:write (fn [s] (reset! !last-output s))}}
+     :!last-output !last-output}))
 
 (defn write [cmdline s]
-  (js/process.stdout.write s))
+  (-> (:process cmdline) .-stdout (.write s)))
+
+(defn last-output [cmdline]
+  @(:!last-output cmdline))
---
+++ src/test/tdd/infra/cmdline_test.cljs
@@ -0,0 +1,10 @@
+(ns tdd.infra.cmdline-test
+  (:require [tdd.infra.cmdline :as x]))
+
+(js/test
+ "creates nullable cmdline"
+ (fn []
+   (let [cmdline (x/create-null)]
+     (x/write cmdline "foo")
+     (-> (js/expect (x/last-output cmdline))
+         (.toBe "foo")))))
