b227818939221b8351648179a2764cc555679c00
Paulus Esterhazy <pesterhazy@gmail.com>
2022-03-17 11:01:21 +0100

    WIP

--- src/test/tdd/infra/cmdline_test.cljs
+++ src/test/tdd/infra/cmdline_test.cljs
@@ -1,10 +1,18 @@
 (ns tdd.infra.cmdline-test
-  (:require [tdd.infra.cmdline :as x]))
+  (:require [tdd.infra.cmdline :as x]
+            jest.matchers))
 
 (js/test
- "creates nullable cmdline"
+ "writes output"
  (fn []
    (let [cmdline (x/create-null)]
      (x/write cmdline "foo")
      (-> (js/expect (x/last-output cmdline))
          (.toBe "foo\n")))))
+
+(js/test
+ "gets args"
+ (fn []
+   (let [cmdline (x/create-null {:args ["a" "b" "c"]})]
+     (-> (js/expect (x/args cmdline))
+         (.toEq ["a" "b" "c"])))))
