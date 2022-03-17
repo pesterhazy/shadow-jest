0ff93113de9442b620faaa098a0490d5f8c5376f
Paulus Esterhazy <pesterhazy@gmail.com>
2022-03-17 10:23:27 +0100

    WIP

---
+++ src/main/tdd/logic/rot13.cljs
@@ -0,0 +1,25 @@
+(ns tdd.logic.rot13
+  (:require [clojure.string :as str]))
+
+(def alphabet-lower-start (.charCodeAt "a" 0) )
+(def alphabet-lower-end (.charCodeAt "z" 0) )
+
+(def alphabet-upper-start (.charCodeAt "A" 0) )
+(def alphabet-upper-end (.charCodeAt "Z" 0) )
+
+(defn xform [c]
+  (let [n (.charCodeAt c 0)]
+    (cond
+      (and (>= n alphabet-lower-start) (<= n alphabet-lower-end))
+      (js/String.fromCharCode (+ (mod (+ (- n alphabet-lower-start) 13) 26)
+                                 alphabet-lower-start))
+      (and (>= n alphabet-upper-start) (<= n alphabet-upper-end))
+      (js/String.fromCharCode (+ (mod (+ (- n alphabet-upper-start) 13) 26)
+                                 alphabet-upper-start))
+      :else
+      c)))
+
+(defn rot13 [s]
+  (->> s
+       (map xform)
+       (str/join "")))
---
+++ src/test/tdd/logic/rot13_test.cljs
@@ -0,0 +1,33 @@
+(ns tdd.logic.rot13-test
+  (:require [tdd.logic.rot13 :as x]
+            [jest.matchers]))
+
+(js/test "handles empty string"
+         (fn []
+           (-> (js/expect (x/rot13 ""))
+               (.toEqual ""))))
+
+(js/test "handles single letter"
+         (fn []
+           (-> (js/expect (x/rot13 "a"))
+               (.toEqual "n"))))
+
+(js/test "handles uppercase letter"
+         (fn []
+           (-> (js/expect (x/rot13 "A"))
+               (.toEqual "N"))))
+
+(js/test "handles numbers"
+         (fn []
+           (-> (js/expect (x/rot13 "1"))
+               (.toEqual "1"))))
+
+(js/test "handles letter after n"
+         (fn []
+           (-> (js/expect (x/rot13 "o"))
+               (.toEqual "b"))))
+
+(js/test "handles multiple letter"
+         (fn []
+           (-> (js/expect (x/rot13 "Hello n1ce World"))
+               (.toEqual "Uryyb a1pr Jbeyq"))))
