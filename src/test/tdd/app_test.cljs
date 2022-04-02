5f743935dca48dbaf7e7bdc20b0633283f338b78
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-02 16:54:09 +0200

    WIP

--- src/test/tdd/app_test.cljs
+++ src/test/tdd/app_test.cljs
@@ -1,27 +1,28 @@
 (ns tdd.app-test
-  (:require [tdd.app :as x]
+  (:require ["@jest/globals" :refer [test expect]]
+            [tdd.app :as x]
             [tdd.infra.cmdline :as cmdline]))
 
-(js/test
+(test
  "runs the app"
  (fn []
    (let [cmdline (cmdline/create-null {:args ["Hello"]})
          app (x/create {:cmdline cmdline})]
      (x/run app)
-     (-> (js/expect (cmdline/last-output cmdline)) (.toBe "Uryyb\n")))))
+     (-> (expect (cmdline/last-output cmdline)) (.toBe "Uryyb\n")))))
 
-(js/test
+(test
  "handles too few args"
  (fn []
    (let [cmdline (cmdline/create-null {:args []})
          app (x/create {:cmdline cmdline})]
      (x/run app)
-     (-> (js/expect (cmdline/last-output cmdline)) (.toBe "Too few args\n")))))
+     (-> (expect (cmdline/last-output cmdline)) (.toBe "Too few args\n")))))
 
-(js/test
+(test
  "handles too many args"
  (fn []
    (let [cmdline (cmdline/create-null {:args ["a" "b"]})
          app (x/create {:cmdline cmdline})]
      (x/run app)
-     (-> (js/expect (cmdline/last-output cmdline)) (.toBe "Too many args\n")))))
+     (-> (expect (cmdline/last-output cmdline)) (.toBe "Too many args\n")))))
