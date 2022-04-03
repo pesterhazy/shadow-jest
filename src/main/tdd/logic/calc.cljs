4373957e9b5b4c6941ce03a017820fc7be420816
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-03 14:47:28 +0200

    WIP

--- src/main/tdd/logic/calc.cljs
+++ src/main/tdd/logic/calc.cljs
@@ -1,7 +1,7 @@
 (ns tdd.logic.calc
   (:require ["regex-escape" :as escape]))
 
-(defn parse-int [s]
+(defn parse-number [s]
   (if (= s "")
     (throw (js/Error. (str "Invalid input: " s)))
     (let [r (js/Number s)]
@@ -16,9 +16,6 @@
      0
      (if-let [m (re-matches #"//(.)\n(.*)" s)]
        (add (m 2) (m 1))
-       (let [segments (map (fn [s]
-                             (parse-int s))
-                           (.split s (re-pattern (str (escape delim) "|\n"))))]
-         (when (empty? segments)
-           (throw (js/Error. (str "Invalid input: " s))))
-         (reduce + 0 segments))))))
+       (->> (.split s (re-pattern (str (escape delim) "|\n")))
+            (map parse-number)
+            (reduce + 0))))))
