eca0ea4e57498afb1d53954e66aa7f9e11d1e158
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-04 19:31:24 +0200

    WIP

--- src/main/tdd/main.cljs
+++ src/main/tdd/main.cljs
@@ -1,4 +1,7 @@
-(ns tdd.main)
+(ns tdd.main
+  (:require [uix.dom.alpha :as uix.dom]
+            [tdd.app :as app]))
 
 (defn init []
-  (prn :main))
+  (uix.dom/render [app/app-ui]
+                  (js/document.querySelector "#app")))
