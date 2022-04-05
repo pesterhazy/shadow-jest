96b97482a90dbbabe747b3ea9839ea1ce55df83c
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-05 16:01:57 +0200

    WIP

--- src/main/tdd/app.cljs
+++ src/main/tdd/app.cljs
@@ -3,7 +3,10 @@
 (defn app-ui []
   [:button "Start"])
 
-(defn board-ui [{:keys [fs]}]
-  (->> (range 9)
-       (map (fn [n] [:div.box {:data-testid "field"} (get fs n)]))
-       (into [:div.board])))
+(defn board-ui [{:keys [fs on-move]}]
+  (let [on-move (or on-move (fn []))]
+    (->> (range 9)
+         (map (fn [n] [:div.box {:data-testid "field"
+                                 :on-click (fn [] (on-move n))}
+                       (get fs n)]))
+         (into [:div.board]))))
--- src/main/tdd/app_test.cljs
+++ src/main/tdd/app_test.cljs
@@ -21,7 +21,7 @@
 (test
  "shows correct fields"
  (fn []
-   (let [fs ["O" "X" "X" "X" "O" "O" "X" "O" "O"]
+   (let [fs ["O" "X" "X" "" "O" "O" "X" "O" "O"]
          _ (rtl/render (uix/as-element [tdd.app/board-ui {:fs fs}]))
          fields (rtl/screen.getAllByTestId "field")]
      (-> (expect (count fields))
@@ -34,3 +34,25 @@
                       (map #(-> % .-innerHTML))
                       vec))
          (.toEq fs)))))
+
+(test
+ "allows user to click on field"
+ (fn []
+   (let [fs ["" "" "" "" "" "" "" "" ""]
+         !n (atom nil)
+         _ (rtl/render (uix/as-element [tdd.app/board-ui
+                                        {:fs fs
+                                         :on-move (fn [n] (reset! !n n))}]))
+         fields (rtl/screen.getAllByTestId "field")]
+     (rtl/fireEvent.click (get fields 2))
+
+     (-> (expect @!n) (.toBe 2)))))
+
+(test
+ "handles missing on-move handler gracefully"
+ (fn []
+   (let [fs ["" "" "" "" "" "" "" "" ""]
+         _ (rtl/render (uix/as-element [tdd.app/board-ui {:fs fs}]))
+         fields (rtl/screen.getAllByTestId "field")]
+     (rtl/fireEvent.click (get fields 2))
+     (-> (expect) .pass))))
