6d5bd21eee75510c87ca4eb74836039b7df2aade
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-06 18:31:04 +0200

    WIP

--- src/main/tdd/main.cljs
+++ src/main/tdd/main.cljs
@@ -2,8 +2,6 @@
   (:require [uix.dom.alpha :as uix.dom]
             [tdd.app :as app]))
 
-(def example-fs ["O" "X" "X" "X" "O" "O" "X" "O" "O"])
-
 (defn init []
   (uix.dom/render [app/app-ui]
                   (js/document.querySelector "#app")))
