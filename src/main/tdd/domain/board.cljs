b13a0ee7b0352ca9cb115c42069a0b1d7bf7fbc7
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-07 23:48:45 +0200

    WIP

--- src/main/tdd/domain/board.cljs
+++ src/main/tdd/domain/board.cljs
@@ -8,12 +8,11 @@
   ([{initial-game :game}]
    (let [[game set-game] (react/useState (or initial-game (game/create)))
          result (game/result game)]
-     [:div
-      [board/board-ui {:fs (game/fields game) ;; use accessor
-                       :frozen (not= :pending result)
-                       :highlight (game/winners game)
-                       :on-move (fn [n]
-                                  (set-game (fn [game] (game/advance game n))))}
-       [board/result-ui {:result result
-                         :on-restart (fn []
-                                       (set-game (game/create)))}]]])))
+     [board/board-ui {:fs (game/fields game) ;; use accessor
+                      :frozen (not= :pending result)
+                      :highlight (game/winners game)
+                      :on-move (fn [n]
+                                 (set-game (fn [game] (game/advance game n))))}
+      [board/result-ui {:result result
+                        :on-restart (fn []
+                                      (set-game (game/create)))}]])))
