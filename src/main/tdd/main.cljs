29c70bde0c2c6aa3dec5801ccd35ae73d99765a8
Paulus Esterhazy <pesterhazy@gmail.com>
2022-04-10 16:22:42 +0200

    WIP

--- src/main/tdd/main.cljs
+++ src/main/tdd/main.cljs
@@ -1,17 +1,4 @@
-(ns tdd.main
-  (:require [uix.dom.alpha :as uix.dom]
-            [tdd.app :as app]
-            [tdd.domain.board :as board]
-            [tdd.logic.game :as game]))
+(ns tdd.main)
 
 (defn ^:dev/after-load init []
-  (case js/location.pathname
-    "/"
-    (uix.dom/render [(fn [] (app/app-ui))] ;; hack for force reloading
-                    (js/document.querySelector "#app"))
-    "/playground/win"
-    (uix.dom/render [board/board-do
-                     {:game (game/create ["X" "X" "X"
-                                          "O" "O" ""
-                                          "" "" ""])}] ;; hack for force reloading
-                    (js/document.querySelector "#app"))))
+  (prn :init))
